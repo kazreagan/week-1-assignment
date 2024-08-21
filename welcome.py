1b03fabb4aacaeda3b2d73e0157f0d75d79c89eb
kazreagan <ouledireagan@gmail.com>
2024-08-21 08:17:55 +0300

    week 1 assignment

---
+++ welcome.py
@@ -0,0 +1,7 @@
+print("Welcome")
+
+name =  input("What is your name?\n")
+age = input("How old are you?\n")
+location = input("Where do you live?\n")
+
+print(f"Hello, {name}! you are {age} years old and live in {location}.")
